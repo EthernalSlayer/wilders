71f64faecd12adacf3ec785a834eea134d2aba02
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-06-29 11:08:57 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+const log = console.log;
+
+log(chalk.bold.red("ALYSON"));
+log(chalk.bold.blue("ISMAEL"));
+log(chalk.bold.green("TRAD"));
+log(chalk.bold.yellow("ALEXANDRA"));
